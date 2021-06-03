141f4a1ff2de9582c3c1f82b4bbdb30b0f0883f5
Ritz GL <maurigarzonlara@hotmail.com>
2021-06-03 13:52:29 +0800

    tables created from schemas successfully

---
+++ db/index.js
@@ -0,0 +1,24 @@
+// ask questions about next action
+    // What do you want to do? 
+        // CREATE   - do first
+        // READ     - second
+        // UPDATE   - last
+        // DELETE   - bonus
+    
+    // Which table would you like to work with?
+        // Department
+        // Role
+        // Employee
+
+
+// Do department first as inquirer
+    // Create a department
+    // Read a department / Read all departments
+// Do roles second
+    // Create a role
+    // Read a role / Read all roles
+// Do Employee last
+    // Create an employee
+    // Read a role / Read all roles
+
+    
--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,48 @@
 -- drop DATABASE create TABLE column names, input type etc
+DROP DATABASE IF EXISTS employees_db;
 
--- drop
--- create 
--- use 
-
+CREATE DATABASE employees_db;
+USE employees_db;
 
 -- create tables 
 -- DO: department, role, employee
 -- in that order
+
+-- 3 tables:
+-- department
+    -- id PRIMARY KEY AUTO INCREMENT INTEGER 
+    -- name VARCHAR(30)
+CREATE TABLE department(
+    id INT AUTO_INCREMENT NOT NULL,
+    dpt_name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+-- role
+    -- id PRIMARY KEY AUTO INCREMENT INTEGER
+    -- title VARCHAR(30)
+    -- salary DECIMAL 
+    -- department_id INTEGER FOREIGN KEY
+CREATE TABLE emp_role(
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id)
+);
+-- employee
+    -- id PRIMARY KEY AUTO INCREMENT INTEGER
+    -- first_name VARCHAR(30)
+    -- last_name VARCHAR(30)
+    -- role_id INTEGER FOREIGN KEY
+    -- manager_id INTEGER FOREIGN KEY
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES emp_role(id),
+    FOREIGN KEY(manager_id) REFERENCES emp_role(id)
+);
