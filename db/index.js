4b201019d9d5e0c6a797440a185d28ff38fa74bd
Ritz GL <maurigarzonlara@hotmail.com>
2021-07-19 13:15:06 +0800

    Seeds created
    Some user prompts created

--- db/index.js
+++ db/index.js
@@ -1,24 +1,76 @@
 // ask questions about next action
-    // What do you want to do? 
-        // CREATE   - do first
-        // READ     - second
-        // UPDATE   - last
-        // DELETE   - bonus
-    
-    // Which table would you like to work with?
-        // Department
-        // Role
-        // Employee
+// What do you want to do?
+// CREATE   - do first
+// READ     - second
+// UPDATE   - last
+// DELETE   - bonus
 
+// Which table would you like to work with?
+// Department
+// Role
+// Employee
 
 // Do department first as inquirer
-    // Create a department
-    // Read a department / Read all departments
+// Create a department
+// Read a department / Read all departments
 // Do roles second
-    // Create a role
-    // Read a role / Read all roles
+// Create a role
+// Read a role / Read all roles
 // Do Employee last
-    // Create an employee
-    // Read a role / Read all roles
+// Create an employee
+// Read a role / Read all roles
 
-    
+const inquirer = require("inquirer");
+
+async function getAction() {
+  let action = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: [
+        "Create department",
+        "Create role",
+        "Create employee",
+        "View departments",
+        "View roles",
+        "View employees",
+      ],
+    },
+  ]);
+  return action;
+}
+
+async function getDepartment(){
+    let department = await inquirer.prompt([
+        {
+            type: "input",
+            name: "departmentName",
+            message: "Enter new department's name: "
+        }
+    ])
+    return department;
+}
+
+async function getRole(){
+    let department = await inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Enter new role's title: "
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "Enter new role's salary: "
+        },
+        // Query database, and pass in the array obtained from that table
+        // Limiting user input, and less chance of misinput
+        {
+            type: "input",
+            name: "departmentID",
+            message: "Enter new role's department ID: "
+        }
+    ])
+    return department;
+}
--- db/seed.sql
+++ db/seed.sql
@@ -1,2 +1,45 @@
 -- INSERT statements with dummy data to test with
 -- this is run to fill the database when queries are done
+
+-- INSERT DEPARTMENTS
+INSERT INTO department
+VALUES(id, "Human Resources");
+
+INSERT INTO department
+VALUES(id,"Information Technology");
+
+INSERT INTO department
+VALUES(id, "Engineering");
+
+-- INSERT ROLES
+-- THESE BELONG TO Human Resources
+
+INSERT INTO emp_role
+VALUES (id, "Advisor", 55000.00, 1);
+
+INSERT INTO emp_role
+VALUES (id, "Consultant", 62000.00, 1);
+
+INSERT INTO emp_role
+VALUES (id, "Occupational Psychologist", 90000.00, 1);
+
+-- THESE BELONG TO Information Technology
+INSERT INTO emp_role
+VALUES (id, "Consultant", 60000.00, 2);
+
+INSERT INTO emp_role
+VALUES (id, "Technician", 75000.00, 2);
+
+INSERT INTO emp_role
+VALUES (id, "Analyst", 80000.00, 2);
+
+-- THESE BELONG TO Engineering
+
+INSERT INTO emp_role
+VALUES (id, "Mech Engineer", 90000.00, 3);
+
+INSERT INTO emp_role
+VALUES (id, "Developer", 67000.00, 3);
+
+INSERT INTO emp_role
+VALUES (id, "Electrical Engineer", 84000.00, 3);
